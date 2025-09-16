5f3616e7f7e0e1f5c9662da228e0ecb77f4f46a1
Jangid Amitkumar <134952776+amitkumar0128@users.noreply.github.com>
2025-09-16 19:25:35 +0530

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -4,13 +4,14 @@ import os
 from pathlib import Path
 
 # Load credentials from .env
-load_dotenv(dotenv_path=Path('/home/jangi/projects/tel_bot/amit_bot/amit/.env'))
+load_dotenv(dotenv_path=Path('./.env'))
 
 api_id = int(os.getenv("API_ID"))
 api_hash = os.getenv("API_HASH")
 bot_token = os.getenv("BOT_TOKEN")
 openai_key = os.getenv("OPENAI_API_KEY")
+bot_name = os.getenv("BOT_USERNAME")
 
 # Initialize the bot client
-bot = TelegramClient("MDL_YT_Bot", api_id, api_hash)
+bot = TelegramClient(bot_name, api_id, api_hash)
 print("Bot Session Started !!")
